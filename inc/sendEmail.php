6aea0e1bad11f54015ce04e2beb1001b17604599
Shah Jeenal <75457847+ShahJ24@users.noreply.github.com>
2021-04-28 20:49:28 -0400

    third commit

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,7 +1,8 @@
 ﻿<?php
 
+
 // Replace this with your own email address
-$siteOwnersEmail = 'user@website.com';
+$siteOwnersEmail = 'jeenal2416@gmail.com';
 
 
 if($_POST) {
@@ -29,7 +30,7 @@ if($_POST) {
 
    // Set Message
    $message .= "Email from: " . $name . "<br />";
-	$message .= "Email address: " . $email . "<br />";
+   $message .= "Email address: " . $email . "<br />";
    $message .= "Message: <br />";
    $message .= $contact_message;
    $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
